dcab4f7113d13f5cb2a0bd84c39a669510749d07
avayedawadi <avaye.dawadi@gmail.com>
2021-07-02 15:36:09 -0400

    Pugh-chart implementation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import './WebComponentCollection/the-gallery'
+import './WebComponentCollection/the-gallery';
+import 'pugh-chart';
 
 if (environment.production) {
   enableProdMode();
